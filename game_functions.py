7b659eb61664b253b355dc05b7cfc32638e97341
EliBater <eb1238@exeter.ac.uk>
2026-01-16 13:24:58 +0000

    Updated pick_value function

--- game_functions.py
+++ game_functions.py
@@ -2,8 +2,14 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    x = random.choice(poss_values)   
-    return x
+    min_x = 0
+    max_x = len(poss_values)-1
+    mid_x = (max_x+min_x)//2
+
+    return poss_values[mid_x]
+
+    #x = random.choice(poss_values)   
+    #return x
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
